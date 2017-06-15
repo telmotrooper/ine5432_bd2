54149abc45292bd59982dc6a22aecb098c2a2c5d
Unknown <jonatajjs@gmail.com>
2017-06-15 17:54:01 -0300

    Add Parser

--- model/parser.py
+++ model/parser.py
@@ -1,7 +1,21 @@
+import os
 
 class Parser():
     def __init__(self, file=None):
         if not None:
-            file = '../arquivos/input.txt'
+            file = '{0}/arquivos/input.txt'.format(os.getcwd())
 
-        
+        self.file = open(file,'r')
+        self.list = self.get_lines()
+
+    def get_lines(self):
+        list = []
+        for line in self.file:
+            list.append(line.strip())
+        return list
+
+    def get_next_inst(self):
+        try:
+            return self.list.pop(0)
+        except:
+            print("Sem mais instruções!")
